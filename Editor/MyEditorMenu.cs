50aaa6b71e7ef311247ed3f64a8df602b3db27d0
badandbest <91832803+badandbest@users.noreply.github.com>
2024-09-25 19:00:38 +0200

    Initial commit

---
+++ Editor/MyEditorMenu.cs
@@ -0,0 +1,10 @@
+using Editor;
+
+public static class MyEditorMenu
+{
+	[Menu( "Editor", "You Can Do It/My Menu Option" )]
+	public static void OpenMyMenu()
+	{
+		EditorUtility.DisplayDialog( "It worked!", "This is being called from your library's editor code!" );
+	}
+}
